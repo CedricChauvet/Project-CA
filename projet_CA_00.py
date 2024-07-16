5478497ace0f3968523bdbe42fe901b40ffafcf0
Cédric, Olivier CHAUVET <chauvetc.ujf@gmail.com>
2024-07-16 18:23:34 +0200

    Add files via upload
    this commit has finished projet_CA_00.py, please use another file to continue

--- projet_CA_00.py
+++ projet_CA_00.py
@@ -49,45 +49,38 @@ class Page(Document):
         self.page=page
         self.id=id
         self.elements = self.get_element()
-        self.size = self.get_size()
+
     def number_lines(self):
         liste1=set()
         for i in self.page:
             liste1.add(i.x0)
 
-        
+    # on peux ajouter des choses comme bbox ici    
     def get_element(self):
         elements=[]
         for elt in self.page:
-            if isinstance(elt, LTTextContainer) :
-               elements.append(Element(elt))
+            if isinstance(elt, LTTextContainer):
+               for text_line in elt:
+                    if isinstance(text_line,LTTextLineHorizontal):         # un poil compilqué mais il faut rentrer dedans pour 
+                        taille_line = next(iter(text_line)).size           # rentrer dans les details, voir bbox et font
+                        # print(dir(LTTextLineHorizontal))                 # peut servir            
+                        elements.append(Element(text_line, taille_line))
         return elements
-
-    def get_size(self):
-        # methode pour obtenir size_car
-        elt_text = next(iter(elt))   # objet de type  LTTextLineHorizontal
-        elt_size = next(iter(elt_text)).size   # cet objet a une size (premier caractere :)
-        return elt_size.size    
+ 
 
 
-class Element(Page):
-
-    def __init__(self, elt):
+class Element(LTTextLineHorizontal):
+    """
+    l'element LTTextLineHorizontal est ce qui se rapproce le plus du texte présent dans le pdf
+    """
+    def __init__(self, elt, size):
         
         self.elt = elt
         self.x0 = self.X_pos
         self.y0 = self.Y_pos
-        # self.color = self.get_color()
-        
+        self.size = size
 
-        if not isinstance(self.elt, LTTextContainer):
-            raise("Element must be a text")
-
-
-    def __repr__(self):
-        return self.elt.get_text()
-        # return self.color + self.elt.get_text()
-    
+    # est utiliser pour organiser et trier les textes
     def __lt__(self, other):
         if self.Y_pos > other.Y_pos:
             return True
@@ -96,12 +89,15 @@ class Element(Page):
         else:
             return False
     
-    # def get_color(self):
-    #    if isinstance(self, LTTextLineHorizontal):
-    #        return '\033[1m'  # bleu
-    #    else:
-    #        return '\033[92m' #vert
-    
+    # print()
+    def __repr__(self):
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        if self.size > 12:
+            color = OKBLUE
+        else: 
+            color =  OKGREEN
+        return color + self.elt.get_text()
 
     @property
     def X_pos(self):
@@ -125,16 +121,11 @@ Docu = Document('Tarifs BForBank2024 V8_WEB.pdf')
 pagesss= Docu.read_doc()
 
 # selection d'elements d'une page
-eltms= pagesss[3].elements
+# eltms= pagesss[3].elements
 
-# affichage des textes de chaque elment
-#print(eltms[:])
-#print("plus ou moins", eltms[5]> eltms[0])
-eltms_tries = sorted(eltms)
-
-#for page in pagesss:
-#    page_triee = sorted(page.elements[:])
-  #  print(page_triee)
+# lecture du codument entier
+for page in pagesss[:]:
+    page_triee = sorted(page.elements[:])
+    print(page_triee)
 
 
-print(dir(LTTextLineHorizontal))
